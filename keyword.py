e231241714ede597e00aebac836160ae11479d6c
Abdulrahman Yaser Osilan <a.osilan@hotmail.com>
2024-10-13 01:22:10 -0400

    Update keyword.py

--- keyword.py
+++ keyword.py
@@ -1,5 +1,7 @@
 from sentence_transformers.cross_encoder import CrossEncoder
 
+df = pd.read_json('example.json')
+
 df['profile_text'] = df[['recruiter_name', 'recruiter_about', 'recruiter_tagline', 'recruiter_education', 'recruiter_linkedin', 'recruiter_currentjob']].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
 
 profiles = df['profile_text'].tolist()
@@ -24,4 +26,5 @@ def get_synonyms(word):
     top_matches = df.sort_values(by='similarity_score', ascending=False).head(20)
     result = top_matches[['recruiter_name', 'recruiter_location', 'recruiter_education', 'recruiter_tagline', 'recruiter_linkedin', 'recruiter_currentjob']]
 
-    result.to_json('keyword.json', orient='records', lines=True)
+    # Save to JSON file
+    return result.to_json('test1.json', orient='records', lines=True)
