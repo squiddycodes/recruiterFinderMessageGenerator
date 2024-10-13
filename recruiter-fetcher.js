d2df9182c0991c319f9284752051d1c1bed5a8d9
Derek Guidorizzi <dguidorizzi@yahoo.com>
2024-10-12 20:46:04 -0400

    working on clicking

--- recruiter-fetcher.js
+++ recruiter-fetcher.js
@@ -73,21 +73,25 @@ const fs = require('fs');
     for(const title of jobTitles){
         //OPEN PAGE
         await page.goto("https://www.linkedin.com/search/results/people/?keywords=" + title + " Recruiter&origin=GLOBAL_SEARCH_HEADER&sid=8V)");
-        await new Promise(r => setTimeout(r, 3000));//extra time for page to load
+        //await new Promise(r => setTimeout(r, 2000));//extra time for page to load
 
         try{
             await page.waitForSelector('.reusable-search__entity-result-list > .reusable-search__result-container', { timeout: 15000 });
         }catch(error){}
         
-        const jobs = await page.$$('.list-style-none > .reusable-search__result-container');
-        for(const job of jobs){
+        const recruiters = await page.$$('.list-style-none > .reusable-search__result-container');
+        for(const recruiter of recruiters){
             let name = "undefined";
             let location = "undefined";
             let status = "undefined";
             let recruiterLinkedin = "undefined";
+            let recruiterAbout = "undefined";
+            let recruiterEducation = "undefined";
+            let recruiterTagline = "undefined";
+            let searchQuery = title;
             try{
-                let element = await job.$(".entity-result__title-text > .app-aware-link > span > span");
-                let recruiterData = await job.evaluate(el => el.textContent.trim().replaceAll("\n", "").replaceAll("\t", ""), element);//remove most whitespace
+                let element = await recruiter.$(".entity-result__title-text > .app-aware-link > span > span");
+                let recruiterData = await recruiter.evaluate(el => el.textContent.trim().replaceAll("\n", "").replaceAll("\t", ""), element);//remove most whitespace
                 recruiterData = recruiterData.split("  ").filter(str => /\w+/.test(str));//split and remove empty strings
                 if(recruiterData.length > 2){//if not a linkedin hidden member
                     name = recruiterData[1].split("View").pop();
@@ -98,11 +102,27 @@ const fs = require('fs');
             }catch(error){}
             try{
                 if(status.includes("Current")){//if they are CURRENTLY a recruiter
+                    const clickable = await recruiter.$('.display-flex');
+                    if (clickable) {
+                        console.log("clicking");
+                        await new Promise(r => setTimeout(r, 2000));//extra time for page to load
+                        await clickable.click();
+                    }
+                    try{
+                        await page.waitForSelector('.text-body-medium', { timeout: 15000 });
+                        let element = await recruiter.$(".text-body-medium");
+                        let recruiterData = await recruiter.evaluate(el => el.textContent.trim().replaceAll("\n", "").replaceAll("\t", ""), element);
+                        recruiterData = recruiterData.split("  ").filter(str => /\w+/.test(str));//split and remove empty strings
+                        recruiterTagline = recruiterData[4];
+                        recruiterStatus = recruiterData[6];
+
+                    }catch(error){}
                     
-                    //visit their page, grab tagline, about, url, education
+                    await page.goBack();//go back when done
+                    //visit their page, grab tagline, about, url, education, update status with current first listed experience
                 }
             }catch(error){}
-            console.log("Name: " + name + "\tLocation: " + location + "\tStatus: " + status + "\n");
+            //console.log("Name: " + name + "\tLocation: " + location + "\tStatus: " + status + "\n");
         }
     }
 
