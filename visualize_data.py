0671986f8ce495dab53600f1b73ea4eb2dd34dec
Abdulrahman Yaser Osilan <a.osilan@hotmail.com>
2024-10-13 02:03:58 -0400

    Update visualize_data.py
    Editing the code and comments

--- visualize_data.py
+++ visualize_data.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from collections import Counter
-import re
-df = pd.read_json('example.json')
 
+
+df = pd.read_json('https://raw.githubusercontent.com/squiddycodes/recruiterFinderMessageGenerator/refs/heads/main/example.json')
 
 df_grouped = df.groupby(['recruiter_education', 'recruiter_location']).size().reset_index(name='count')
 
+# Sort by count to get the top combinations
 df_top10 = df_grouped.sort_values(by='count', ascending=False).head(7)
 
-df_top10['count'] = df_top10['count'].astype(int)
-
-# Plot the top 10 most common combinations of recruiter education and location
-plt.figure(figsize=(12, 8))
+# Plot the most common combinations of recruiter education and location
+plt.figure(figsize=(24, 16))
 sns.barplot(x='count', y='recruiter_education', hue='recruiter_location', data=df_top10)
-plt.title('Top 10 Most Common Recruiter Education and Locations')
+plt.title('Top 7 Most Common Recruiter Education and Locations')
 plt.xlabel('Count')
 plt.ylabel('Recruiter Education')
 plt.legend(title='Location', bbox_to_anchor=(1.05, 1), loc='upper left')
